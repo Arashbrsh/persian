031f7aabce544976b8c2d3197204218087c9026c
Arashbrsh <r3v3ng3r1111@gmail.com>
2015-12-20 19:00:25 +0330

    Create seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,9 +220,9 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
+    sudo_users = {152485254,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
-    realm = {},--Realms Id
+    realm = {60156627},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 

e9a42764c7d1b6667aa9a8a5498d2a9913cb12d2
Arashbrsh <r3v3ng3r1111@gmail.com>
2015-12-29 12:13:07 +0330

    Create inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -4,13 +4,13 @@ function run(msg, matches)
 	 local data = load_data(_config.moderation.data)
     if matches[1] == 'join' and data[tostring(matches[2])] then
   		if is_banned(msg.from.id, matches[2]) then
-	 		  return 'You are banned.'
+	 		  return 'شما بن شدید.'
 	  	end
       if is_gbanned(msg.from.id) then
-		    return 'You are globally banned.'
+		    return 'شما گلوبال بن شدید.'
       end
       if data[tostring(matches[2])]['settings']['lock_member'] == 'yes' and not is_owner2(msg.from.id, matches[2]) then
-        return 'Group is private.'
+        return 'گروه خصوصی است'
       end
       local chat = "chat#id"..matches[2]
 		  local user = "user#id"..msg.from.id
